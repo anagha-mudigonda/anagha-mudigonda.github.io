1090586441226176ae8a000068ec4f4e7c88a305
anagha-mudigonda <anagha1602@gmail.com>
2020-07-03 16:42:57 -0700

    javascript updated

--- assets/js/main.js
+++ assets/js/main.js
@@ -284,7 +284,21 @@ Index All JS
         }
     })
 
+   /*--------------------------------
+        13. Scroll position
+    ---------------------------------*/
 
+    const article = document.querySelector("article");
+    const progressBar = document.querySelector(".progress");
+
+    window.addEventListener("scroll", () => {
+    let scrollValue = window.scrollY;
+    let articleHeight = article.clientHeight - window.innerHeight;
+
+    let progressPercentage = (scrollValue / articleHeight) * 100;
+
+    progressBar.style.width = progressPercentage + "%";
+    });
 
 
 
